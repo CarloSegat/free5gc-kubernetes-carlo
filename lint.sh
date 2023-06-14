f439b689832a4095405c9124de4108f49b001b06
Hai Dinh Tuan <haidinhtuan@gmail.com>
2023-06-14 20:46:05 +0200

    update Helm chart

---
+++ lint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run helm lint on charts inside ./charts/free5gc/charts/
+helm lint ./charts/free5gc/charts/*
+
+# Run helm lint on charts inside ./charts/
+helm lint ./charts/*
+
+# Package charts and place them in ./repo/
+helm package --destination ./repo/ ./charts/*
+
+# Generate Helm repository index
+helm repo index --url https://haidinhtuan.github.io/free5gc-kubernetes/ .
+
+# Commit changes to Git repository
+git add .
+git commit -m "update Helm chart"
+git push
+
