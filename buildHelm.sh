c7b90f2e01641a2f9c7164490a74c219946e9708
Carlo Segat (TUB) <carlo.segat@tu-berlin.de>
2025-03-11 12:29:49 +0100

    update Helm chart

---
+++ buildHelm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run helm lint on charts inside ./charts/free5gc/charts/
+helm lint ./charts/free5gc/charts/*
+
+# Run helm lint on charts inside ./charts/
+helm lint ./charts/*
+
+# Package charts and place them in ./repo/
+helm package --destination ./repo/ ./charts/*
+
+# Generate Helm repository index
+helm repo index --url https://github.com/CarloSegat/free5gc-kubernetes-carlo .
---
+++ push.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git add .
+git commit -m "update Helm chart"
+git push
